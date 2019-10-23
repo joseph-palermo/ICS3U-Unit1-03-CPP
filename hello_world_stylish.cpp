775c99b9a1fa815b9cc74371ec0712ab5c0b2721
Ubuntu <ubuntu@ip-172-31-31-42.ec2.internal>
2019-10-23 23:07:17 +0000

    Initial commit

---
+++ hello_world_stylish.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2019 Joseph Palermo All rights reserved.
+//
+// Created by: Joseph Palermo
+// Created on: September 2019
+// This is the "Hello, World!" program, but stylish
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+}
